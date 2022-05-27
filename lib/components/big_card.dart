ba815d9df317ddac497f0fbe895e1dd87b970331
Avaneesh Chopdekar <avaneeshchopdekar42@gmail.com>
2022-05-27 16:32:24 +0530

    Added ripple effect to homepage cards

--- lib/components/big_card.dart
+++ lib/components/big_card.dart
@@ -13,30 +13,33 @@ class BigCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Container(
-        decoration: BoxDecoration(
-            image:
-                DecorationImage(image: AssetImage(bgImage), fit: BoxFit.cover),
-            borderRadius: BorderRadius.circular(12)),
-        height: MediaQuery.of(context).orientation == Orientation.portrait
-            ? 150
-            : MediaQuery.of(context).size.height - 30,
-        width: MediaQuery.of(context).orientation == Orientation.portrait
-            ? MediaQuery.of(context).size.width - 80
-            : 180,
+      child: Material(
+        borderRadius: BorderRadius.circular(12),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
         child: InkWell(
+          splashColor: Colors.black38,
           onTap: () {
             Navigator.push(
                 context, MaterialPageRoute(builder: ((context) => page)));
           },
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(12)),
-            child: Center(
-              child: Text(
-                text,
-                style: const TextStyle(color: Colors.white, fontSize: 40),
+          child: Ink.image(
+            fit: BoxFit.cover,
+            image: AssetImage(bgImage),
+            height: MediaQuery.of(context).orientation == Orientation.portrait
+                ? 150
+                : MediaQuery.of(context).size.height - 30,
+            width: MediaQuery.of(context).orientation == Orientation.portrait
+                ? MediaQuery.of(context).size.width - 80
+                : 180,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                child: Text(
+                  text,
+                  style: const TextStyle(color: Colors.white, fontSize: 40),
+                ),
               ),
             ),
           ),
