be7b5e8f013d9720a52a582a747737f73f27b3ea
Avaneesh-Chopdekar <89315115+Avaneesh-Chopdekar@users.noreply.github.com>
2022-07-26 01:11:17 +0530

    Fixed ms chouhan pdf issue

--- lib/pages/jeebooks/chem_jeebooks.dart
+++ lib/pages/jeebooks/chem_jeebooks.dart
@@ -22,7 +22,7 @@ class _ChemJeeBooksState extends State<ChemJeeBooks> {
     'https://1drv.ms/b/s!AmuzjkvH34D_e6KOg8yGtu_UCnM?e=otHUxc', // MTG Fingertips
     'https://drive.google.com/file/d/1Rno6HV-Wlva2s2KRvSijx7WuOabLawgV/view', // Arihant
     'https://drive.google.com/file/d/111zT7mROCu9YSfnWeo1iTZOIe2vTr8W6/view', // Errorless
-    'https://1drv.ms/b/s!AmuzjkvH34D_fLN2ME_ypuHQK1g', // M.S.Chouhan JEE
+    'https://drive.google.com/file/d/1s-8BQRBpKP5VoySdUHCx2LhdPU-0ui9l/view?usp=drivesdk', // M.S.Chouhan JEE
     'https://drive.google.com/file/d/1NL5GYJeTPEvqSJ_hUVkIpd1f_w1qr7dS/view', // Himanshu Pandey
     'https://drive.google.com/file/d/1fciOkvocr3cgIzmRWLzlF3bBz6ts6rOX/view', // V K Jaiswal 1
     'https://drive.google.com/file/d/1LtPwWqEXrH5B5Lguis9zqZ6U-9omKWY2/view', // V K Jaiswal 2
