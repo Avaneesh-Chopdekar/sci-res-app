25b554ecb0fda3e76826990fa9c54278c8d0176b
Avaneesh-Chopdekar <89315115+Avaneesh-Chopdekar@users.noreply.github.com>
2022-07-27 01:36:16 +0530

    changed website to app in more info

--- lib/pages/more_info.dart
+++ lib/pages/more_info.dart
@@ -9,7 +9,7 @@ class MoreInfo extends StatelessWidget {
       'You need internet connection to use this app.',
       'H.C.Verma is banned in Google Drive that\'s why I used Microsoft One Drive.',
       'It is recommended for you to have Microsoft One Drive App installed for better experience.',
-      'Content of this website belongs to their respective owners.',
+      'Content of this app belongs to their respective owners.',
       'Copyright Disclaimer under Section 107 of the copyright act 1976, allowance is made for fair use for purposes such as criticism, comment, news reporting, scholarship, and research. Fair use is a use permitted by copyright statute that might otherwise be infringing. Non-profit, educational or personal use tips the balance in favour of fair use.',
       'Computer Science Books\' Pdfs are unavailable.',
       'This app is made by Avaneesh Chopdekar.',
